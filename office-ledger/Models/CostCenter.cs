114ac731e2a90d0efe10220d6d3ff4cf3718dabe
owais.khan <owais.khan@SW-OWAISKHAN>
2020-11-26 11:11:16 +0500

    cost-centers-api

---
+++ office-ledger/Models/CostCenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace office_ledger.Models
+{
+    public class CostCenter
+    {
+        public String cost_id { get; set; }
+        public String cost_code { get; set; }
+
+        public String cost_name { get; set; }
+
+        public String user_cd { get; set; }
+
+        public String row_id { get; set; }
+
+        public String updUser_cd { get; set; }
+
+        public String action { get; set; }
+
+        public String dataSet { get; set; }
+
+
+
+    }
+}
