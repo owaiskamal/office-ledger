e423af007493d6ea468658970928bed9114f0f12
owais.khan <owais.khan@SW-OWAISKHAN>
2020-11-30 17:10:39 +0500

    custMaster

---
+++ office-ledger/Models/CustomerMaster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace office_ledger.Models
+{
+    public class CustomerMaster
+    {
+        public String p_Cust_Id { get; set; }
+
+        public String p_cust_No { get; set; }
+
+        public String p_cust_name { get; set; }
+
+        public String p_contact_person { get; set; }
+        public String p_contact_no { get; set; }
+
+        public String p_email { get; set; }
+
+        public String p_user_cd { get; set; }
+
+        public String p_COANO { get; set; }
+
+        public String p_upddate_cd { get; set; }
+
+        public String p_rowid { get; set; }
+
+        public String p_action { get; set; }
+
+        public String p_dataset { get; set; }
+    }
+
+
+}
