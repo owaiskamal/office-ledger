587515cff8cecf8ac90e13058f103d8ffa0b071e
usama <usama@SW-USAMA>
2020-12-01 15:43:41 +0500

    Bank Account api

---
+++ office-ledger/Models/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace office_ledger.Models
+{
+    public class BankAccount
+    {
+        public String bankNo { get; set; }
+
+        public String bankCode { get; set; }
+
+        public String bankName { get; set; }
+
+        public String userCd { get; set; }
+
+        public String row_id { get; set; }
+
+        public String upUserCd { get; set; }
+
+        public String action { get; set; }
+
+        public String dataset { get; set; }
+    }
+}
